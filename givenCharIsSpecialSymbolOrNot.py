e4240839cacc4f009017f50d5a3348cd03361deb
Tang0charli3 <darshanynvfx@gmail.com>
2023-05-12 18:24:37 +0530

    Python

---
+++ givenCharIsSpecialSymbolOrNot.py
@@ -0,0 +1,6 @@
+# WAP to check given char is Special symbol or not.
+a='a'
+if a in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0987654321":
+    print("Not a Special Char")
+else:
+    print("Special Char")
---
+++ numberOfElementPresentIsEvenOrOdd.py
@@ -0,0 +1,6 @@
+# WAP to check number of elements present in list, if it is even-print Good morning else print good evening
+a = [10, 20, 30, 40, 50]
+if len(a)%2 == 0:
+    print("Good Morning")
+else:
+    print("Good Evening")
